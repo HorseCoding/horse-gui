8458fa5d3673370585ce0a4243a3cbc52a5f2aaa
dumorando <144301348+dumorando@users.noreply.github.com>
2024-05-23 23:43:16 -0400

    remove cross-env

--- cloudflare/cloudflarescript.sh
+++ cloudflare/cloudflarescript.sh
@@ -2,5 +2,4 @@
 cd ..
 npm i -g pnpm@v6
 pnpm i --shamefully-hoist
-npm i -g cross-env
-cross-env NODE_OPTIONS=--openssl-legacy-provider NODE_ENV=production webpack --bail
+NODE_OPTIONS=--openssl-legacy-provider NODE_ENV=production npx webpack --bail
