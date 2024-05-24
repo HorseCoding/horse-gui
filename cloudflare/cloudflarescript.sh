695814b6d97ffeeb00883845fb8dc3daecccec93
dumorando <144301348+dumorando@users.noreply.github.com>
2024-05-23 23:53:49 -0400

    in my imagination your waiting lying on your side

--- cloudflare/cloudflarescript.sh
+++ cloudflare/cloudflarescript.sh
@@ -2,4 +2,14 @@
 cd ..
 npm i -g pnpm@v6
 pnpm i --shamefully-hoist
+#this shit better install
+npm install -D webpack-cli
+npm install -D webpack-cli
+npm install -D webpack-cli
+npm install -D webpack-cli
+npm install -D webpack-cli
+npm install -D webpack-cli
+npm install -D webpack-cli
+npm install -D webpack-cli
+npm install -D webpack-cli
 NODE_OPTIONS=--openssl-legacy-provider NODE_ENV=production pnpm dlx webpack -y --bail
