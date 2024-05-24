993e2f6e512551406d8d9bfb94102cede34dee11
dumorando <144301348+dumorando@users.noreply.github.com>
2024-05-23 23:55:14 -0400

    with ur hands between ur thighs

--- cloudflare/cloudflarescript.sh
+++ cloudflare/cloudflarescript.sh
@@ -3,13 +3,13 @@ cd ..
 npm i -g pnpm@v6
 pnpm i --shamefully-hoist
 #this shit better install
-npm install -D webpack-cli
-npm install -D webpack-cli
-npm install -D webpack-cli
-npm install -D webpack-cli
-npm install -D webpack-cli
-npm install -D webpack-cli
-npm install -D webpack-cli
-npm install -D webpack-cli
-npm install -D webpack-cli
+npm install -D webpack-cli --legacy-peer-deps --force
+npm install -D webpack-cli --legacy-peer-deps --force
+npm install -D webpack-cli --legacy-peer-deps --force
+npm install -D webpack-cli --legacy-peer-deps --force
+npm install -D webpack-cli --legacy-peer-deps --force
+npm install -D webpack-cli --legacy-peer-deps --force
+npm install -D webpack-cli --legacy-peer-deps --force
+npm install -D webpack-cli --legacy-peer-deps --force
+npm install -D webpack-cli --legacy-peer-deps --force
 NODE_OPTIONS=--openssl-legacy-provider NODE_ENV=production pnpm dlx webpack -y --bail
