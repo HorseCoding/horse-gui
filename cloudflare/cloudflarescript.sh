99d8307e51365f8f86ab266f332834f5bb96abe3
dumorando <144301348+dumorando@users.noreply.github.com>
2024-05-25 13:59:55 -0400

    lets just use gh pages

--- cloudflare/cloudflarescript.sh
+++
@@ -1,7 +0,0 @@
-# builds the website
-cd ..
-npm i -g pnpm@v6
-pnpm i --shamefully-hoist
-#this shit better install
-npm install -D webpack-cli --legacy-peer-deps --force
-NODE_OPTIONS=--openssl-legacy-provider NODE_ENV=production pnpm dlx webpack -y --bail
